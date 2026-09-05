1a24a40ddc30455ddeec3c3f14aaa12c35c48967
Islom <xakimovislom824@gmail.com>
2026-09-05 16:15:47 +0500

    fuu

--- store/store.ts
+++ store/store.ts
@@ -1,14 +1,37 @@
-  import { configureStore } from "@reduxjs/toolkit";
-  import { baseApi } from "../services/baseApi";
 
-  export const store = configureStore({
-    reducer: {
-      [baseApi.reducerPath]: baseApi.reducer,
+// services/baseApi.ts
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  reducerPath: "baseApi",
+
+  baseQuery: fetchBaseQuery({
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
+    prepareHeaders: (headers) => {
+      const token = localStorage.getItem("access");
+
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+
+      return headers;
     },
-    middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(baseApi.middleware),
-  });
+  }),
 
-  // types
-  export type RootState = ReturnType<typeof store.getState>;
-  export type AppDispatch = typeof store.dispatch;
+  tagTypes: [
+    "Wishlist",
+    "Comparisons",
+    "Profile",
+    "Payments",
+    "Product",
+    "User",
+    "Favorite",
+    "Cart",
+    "Feedback",
+    "Contact",
+    "ProductDetail",
+    "OrderAddress",
+  ],
+
+  endpoints: () => ({}),
+}); 
